a9859cdc29af4e25b76b82fa33e1c470ae403e5c
Myoungok Kim <cheese0414@naver.com>
2016-09-27 20:40:30 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emirim/cheese0414/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/imagetest/MainActivity.java
@@ -7,12 +7,41 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    MyImage mImg;
+    public static final int ORIGINAL=0;
+    public static final int ROTATE=1;
+    public static final int TRANSLAATE=2;
+    public static final int SCALE=3;
+    public static final int SKEW=4;
+    int choose=ROTATE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linear1=(LinearLayout)findViewById(R.id.linear1);
+        mImg=new MyImage(getApplicationContext());
+        linear1.addView(mImg);
+    }
+    protected void transformImage(View v) {
+        switch(v.getId()) {
+            case R.id.but_rotate:
+                choose=ROTATE;
+                break;
+            case R.id.but_translate:
+                choose=TRANSLAATE;
+                break;
+            case R.id.but_scale:
+                choose=SCALE;
+                break;
+            case R.id.but_skew:
+                choose=SKEW;
+                break;
+        }
+        mImg.setChoose(choose);
+        mImg.invalidate(); //MyImage.java에 있는 onDraw()를 다시 호출함
     }
 }
--- app/src/main/java/kr/hs/emirim/cheese0414/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/imagetest/MyImage.java
@@ -10,19 +10,41 @@ import android.view.View;
  * Created by appcreate02 on 2016. 9. 26..
  */
 public class MyImage extends View {
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
     MyImage(Context context) {
         super(context);
+        picture= BitmapFactory.decodeResource(getResources(), R.drawable.semi);
+    }
+
+    public void setChoose(int choose) {
+        this.choose = choose;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.semi);
 
         float cx=getWidth()/2.0f;
         float cy=getHeight()/2.0f;
         float x=(getWidth()-picture.getWidth())/2.0f;
         float y=(getHeight()-picture.getHeight())/2.0f;
+        switch (choose) {
+            case MainActivity.ROTATE:
+                canvas.rotate(45, cx, cy);
+                break;
+            case MainActivity.TRANSLAATE:
+                canvas.translate(-150, 200);
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(0.5f, 0.8f, cx, cy);
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f, 0.4f);
+                break;
+
+        }
         //회전
         //canvas.rotate(45, cx, cy);
         //이동
@@ -32,5 +54,6 @@ public class MyImage extends View {
         //비틀기
         //canvas.skew(0.4f, 0.4f);
         canvas.drawBitmap(picture, x, y, null);
+        //picture.recycle();
     }
 }
